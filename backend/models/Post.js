49afaa858332beebf9624a075676950134051da8
Mikine197 <shekrubel754@gmail.com>
2025-10-01 00:27:57 +0600

    Create Post.js

---
+++ backend/models/Post.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    userId: String,
+    mediaUrl: String,
+    caption: String,
+    likes: [String],
+    comments: [{ userId: String, text: String }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);
